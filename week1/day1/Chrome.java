c4f94f9e20258d05b5035af611b5de19b1efb575
SenthilNS13 <senthilsaravananns@gmail.com>
2024-12-08 07:53:17 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+public static void main(String[] args) {
+	float degree=91.0f;
+	String developer="Google";
+	boolean beta=false;
+	int year=2008;
+	short  type='C';
+	System.out.println("your degree is "+ degree);
+	System.out.println("your developer  is"+ developer);
+	System.out.println("Are you nee to Test Leaf"+ beta);
+	System.out.println("Which year your born"+ year);
+	System.out.println("Favourite Alphabet"+ type);
+	
+	
+}
+}
